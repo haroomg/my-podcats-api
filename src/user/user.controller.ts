29b7c40dd5e9f2e45bd901c903ed2b4b7578f10c
lharo <haroomg@gmail.com>
2024-11-20 15:31:01 -0400

    we made some changes in the user rigth now we have a semi crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,38 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Query,
+  Get,
+  Param,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './entity/user.entity';
+import { IsPositivePipe } from 'src/pipes/is-positive.pipe';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @Get()
+  async findAll(
+    @Query('orederBy') orderBy: 'asc' | 'desc' = 'asc',
+    @Query('limit', new DefaultValuePipe(100), ParseIntPipe, IsPositivePipe)
+    limit: number,
+  ) {
+    return this.userService.findAll(orderBy, limit);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
-    return this.userService.createUser(createUserDto);
+    return this.userService.create(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -8,7 +14,50 @@ import { UserEntity } from './entity/user.entity';
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(data: CreateUserDto): Promise<UserEntity> {
+  async findAll(
+    orderBy: 'asc' | 'desc' = 'asc',
+    limit: number = 10,
+  ): Promise<Array<UserEntity>> {
+    let userList: Array<UserEntity> = [];
+    const users = await this.prisma.user.findMany({
+      orderBy: {
+        id: orderBy,
+      },
+    });
+
+    for (let i = 0; i < users.length; i++) {
+      let user = users[i];
+
+      let userEntity: UserEntity = {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.createdAt,
+      };
+      userList.push(userEntity);
+    }
+    return userList.slice(0, limit);
+  }
+
+  async findOne(id: number): Promise<UserEntity> {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User  with ID ${id} not found`);
+    }
+
+    const userEntity: UserEntity = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.createdAt,
+    };
+    return userEntity;
+  }
+
+  async create(data: CreateUserDto): Promise<UserEntity> {
     const username: string = data.username;
     const email: string = data.email;
     const password: string = data.password;
@@ -25,8 +74,9 @@ export class UserService {
     if (existsEmail) {
       throw new ConflictException('Email already exists');
     }
+
     if (password !== confirmPassword) {
-      throw new Error('Passwords do not match');
+      throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST);
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
